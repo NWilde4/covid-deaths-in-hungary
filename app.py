b9cd0a6774ea044e20dcda044a1099a293e11ab3
NWilde4 <wilde.norman@yahoo.com>
2021-03-31 09:59:25 +0200

    add print statements

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import requests
 import bs4
 import csv
+import time
 
 counter = 1
 last_page_not_reached = True
 age_dictionary = {age:0 for (age) in range(121)}
+
+print('Program execution may take a few minutes.')
+print('Program will close automatically when the csv file is ready.')
+print('Loading...')
+time.sleep(5)
 
 while last_page_not_reached:
   url = f'https://koronavirus.gov.hu/elhunytak?page={counter}'
@@ -15,11 +21,12 @@ while last_page_not_reached:
   for age in age_list:
     formatted_age = int(age.getText().strip())
     age_dictionary[formatted_age] += 1
-
+  print(f'{counter} pages parsed already.')
   # Check if last page has been reached
   if not (soup.find('li', class_='pager-last')):
     last_page_not_reached = False
   counter += 1
+
 
 field_names = ['age', 'frequency']
 with open('covid_deaths.csv', 'w') as f:
